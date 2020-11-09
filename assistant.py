a19286f8a62cac74537c9957d3a6f6908b8cb29a
RestKluge <iqweenz01@gmail.com>
2020-11-10 02:17:39 +0700

    update: system tray icon

--- assistant.py
+++ assistant.py
@@ -34,6 +34,7 @@ from youtube_search import YoutubeSearch
 
 from assistant_threads import *
 from mainwindow import Ui_MainWindow
+from PyQt5.Qt import *
 
 drive = "C:/"
 chain = []
@@ -48,6 +49,30 @@ def find_app(name, path):
 class Assistant():
     def __init__(self):
         app = QtWidgets.QApplication(sys.argv)
+        app.setQuitOnLastWindowClosed(False)
+
+        icon = QIcon(
+            "./icon/fugue-icons-3.5.6/icons/application-plus-black.png")
+        tray = QSystemTrayIcon()
+        tray.setVisible(True)
+        tray.setIcon(icon)
+
+        menu = QMenu()
+        action1 = QAction("About")
+        # action1.triggered.connect()
+        menu.addAction(action1)
+
+        action2 = QAction("Settings")
+        # action2.triggered.connect()
+        menu.addAction(action2)
+
+        aquit = QAction("Exit")
+        aquit.triggered.connect(app.quit)
+        menu.addAction(aquit)
+
+        tray.setContextMenu(menu)
+        tray.activated.connect(self.activate)
+
         self.MainWindow = QtWidgets.QMainWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.MainWindow)
@@ -66,6 +91,13 @@ class Assistant():
         self.MainWindow.setWindowFlag(Qt.WindowStaysOnTopHint)
         self.MainWindow.setFixedSize(
             self.MainWindow.width(), self.MainWindow.height())
+
+    def activate(self, reason):
+        if reason == QSystemTrayIcon.Trigger:
+            self.MainWindow.show()
+
+    def exit_window(self):
+        self.MainWindow.close()
 
     # Đặt text cho PlainTextEdit
     def set_plain_text_edit(self, text):
